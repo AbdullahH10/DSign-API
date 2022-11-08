0cf5a5c67db1d72f80e90218c3068411f1f39d86
AbdullahH10 <abdullahh10@outlook.com>
2022-11-08 10:38:36 +0600

    CORS configured globally

--- src/main/java/com/example/dsignapi/controller/UserController.java
+++ src/main/java/com/example/dsignapi/controller/UserController.java
@@ -17,7 +17,6 @@ import java.util.List;
 import java.util.Optional;
 //@RequestMapping("/image")
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
     @Autowired
     private UserService service;
